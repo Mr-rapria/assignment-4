d99fcbd80ed272f291793214849382dd7ebafcec
Mr-rapria <97610540+Mr-rapria@users.noreply.github.com>
2022-03-01 23:11:13 +0530

    Add files via upload

---
+++ Assignment4_output.py
@@ -0,0 +1,53 @@
+ANSWER 1
+Move disk 1 from rod A to rod C
+Move disk 2 from rod A to rod B
+Move disk 1 from rod C to rod B
+Move disk 1 from rod B to rod A
+Move disk 2 from rod B to rod C
+Move disk 1 from rod A to rod C
+
+
+ANSWER 2
+Enter the size of pascals triangle 5
+USING FOR LOOP
+      1
+     1 1
+    1 2 1
+   1 3 3 1
+  1 4 6 4 1
+
+
+
+USING WHILE LOOP
+      1
+     1 1
+    1 2 1
+   1 3 3 1
+  1 4 6 4 1
+
+
+USING RECURSSIONS
+        1
+      1   1
+    1   2   1
+  1   3   3   1
+1   4   6   4   1
+
+
+
+ANSWER 3
+Enter 2 numbers: 5 7
+a. Checking if the quotient and remainder is a callFalse
+False
+<b> The quotient is zero
+<c> Filtered out numbers that are greater than 4 are : [9, 10, 5, 10, 6, 11]
+<d> Set: {5, 6, 9, 10, 11}
+<e> Immutable set: frozenset({5, 6, 9, 10, 11})    
+ 11
+
+
+ANSWER 6
+Enter the word: Roman
+Enter a new meaningful word using the exact same letters to test your friendship: Monar
+Does the word makes sense?(y/Y or n/N )N
+The word doesn't have a meaning, friendship is fake
